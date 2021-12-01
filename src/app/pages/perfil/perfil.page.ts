5a132925cfe94d8a0e687fbadd9dc1121bfcdce9
xluis1973 <50953075+xluis1973@users.noreply.github.com>
2021-12-01 00:05:48 -0300

    Perfil. Guarda datos del perfil

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -1,22 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Usuario, Visitante } from 'src/app/interfaces/interfaces';
 import { NavController } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
+import { AutorizaService } from 'src/app/services/autoriza.service';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.page.html',
   styleUrls: ['./perfil.page.scss'],
 })
-export class PerfilPage implements OnInit {
+export class PerfilPage implements OnInit, AfterViewInit {
 
+  botonDesactivado=true;
   usuario:Usuario={
     identificador:"",
-    apellido: "Sartoris",
-    nombre:"Gabriela",
-    domicilio:"Mod. 11 mz. 22 c.18",
-    ciudad: "La Punta",
-    telefono:"154851526",
+    apellido: "",
+    nombre:"",
+    domicilio:"",
+    ciudad: "",
+    telefono:"",
     activo:true
   }
   visitante:Visitante={
@@ -27,22 +29,36 @@ export class PerfilPage implements OnInit {
 
 
   };
-  constructor(private navCtrl:NavController, private activatedRoute:ActivatedRoute) { }
+  constructor(private navCtrl:NavController, private activatedRoute:ActivatedRoute, 
+    private autorizaService:AutorizaService) { }
+  ngAfterViewInit(): void {
+    //this.autorizaService.obtenerUsuario(this.usuario, this.visitante);
+  }
 
-  ngOnInit() 
+  async ngOnInit() 
 {
   this.activatedRoute.queryParams.subscribe(
     param=>{
       this.usuario.identificador=param.identificador;
       this.usuario.apellido=param.apellido;
       this.usuario.nombre=param.nombre;
+      this.visitante.identificador=this.usuario.identificador;
+      this.visitante.usuario=this.usuario.identificador;
      
     }
   );
 
+  await this.autorizaService.obtenerUsuario(this.usuario, this.visitante);
+
+  this.botonDesactivado=false;
 
   }
+  
 confirmar(formulario){
+
+  
+
+  this.autorizaService.guardarDatos(this.usuario,this.visitante);
   this.navCtrl.navigateRoot("/visitante",{animated:true});
 
 }
--- src/app/services/autoriza.service.ts
+++ src/app/services/autoriza.service.ts
@@ -2,17 +2,22 @@ import { Injectable } from '@angular/core';
 import { GooglePlus } from '@ionic-native/google-plus/ngx';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { ToastController } from '@ionic/angular';
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword  } from "firebase/auth";
+import { getAuth  } from "firebase/auth";
 import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from 'src/environments/environment.prod';
+import { getFirestore, getDocs, collection,setDoc,doc, query, where, getDoc } from 'firebase/firestore/lite';
+
+import { Usuario, Visitante } from '../interfaces/interfaces';
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
+const db = getFirestore(app);
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutorizaService {
+
 
   constructor(private gp:GooglePlus, private AFAuth:AngularFireAuth,private toastCtrl:ToastController) { }
 
@@ -33,5 +38,64 @@ export class AutorizaService {
     
   }
 
+  async  guardarDatos(usuario:Usuario,visitante:Visitante) {
+
+    //console.log('Guardando');
   
+    const usuarioCol=doc(db, "usuario", usuario.identificador );
+    const visitanteCol=doc(db, "visitante", visitante.identificador );
+    await setDoc(usuarioCol, usuario).catch((error)=>{
+  
+      console.log('Error al guardar Usuario ',error.message);
+  
+    });
+
+    await setDoc(visitanteCol, visitante).catch((error)=>{
+  
+      //console.log('Error al guardar Visitante ',error.message);
+  
+    });
+  
+    
+}
+
+async obtenerUsuario(usuario:Usuario,visitante:Visitante){
+  //Obteniendo Usario
+  const usuarioCol = collection(db, 'usuario');
+
+  const q = query(usuarioCol, where("identificador", "==", usuario.identificador));
+  const usuarioSnapshot = await getDocs(q);
+  
+  const usuarioList = usuarioSnapshot.docs.map(doc => doc.data());
+  usuarioList.forEach((user)=>{
+    usuario.identificador=user.identificador;
+    usuario.activo=user.activo;
+    usuario.apellido=user.apellido;
+    usuario.ciudad=user.ciudad;
+    usuario.domicilio=user.domicilio;
+    usuario.nombre=user.nombre;
+    usuario.telefono=user.telefono;
+    //console.log("usuario ",user);
+    
+  
+  });
+
+  //Obteniendo visitante
+  const visitanteCol = collection(db, 'visitante');
+
+  const qVisitante = query(visitanteCol, where("identificador", "==", usuario.identificador));
+  const visitanteSnapshot = await getDocs(qVisitante);
+  
+  const visitanteList = visitanteSnapshot.docs.map(doc => doc.data());
+  visitanteList.forEach((user)=>{
+    visitante.identificador=user.identificador;
+    visitante.pais=user.pais;
+    visitante.provincia=user.provincia;
+    visitante.usuario=user.usuario;
+    //console.log("visitante ",user);
+    
+  
+  });
+
+}
 }
